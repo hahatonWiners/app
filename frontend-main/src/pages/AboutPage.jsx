a42ecbe2e130df3000707c9797f469090864313c
almond <almondawwawa@gmail.com>
2025-04-19 05:39:43 +0300

    Update descriptions

--- frontend-main/src/pages/AboutPage.jsx
+++ frontend-main/src/pages/AboutPage.jsx
@@ -3,32 +3,32 @@ import './AboutPage.css';
 
 const teamMembers = [
   {
-    name: 'Михаил Носков',
-    role: 'Капитан команды',
+    name: 'Носков Михаил',
+    role: 'ML, Капитан команды',
     description: 'Описание 1',
     image: '/avatars/member1.jpg',
   },
   {
-    name: 'Герман',
-    role: 'Роль 2',
+    name: 'Лошков Дмитрий',
+    role: 'CV, Дизайнер',
     description: 'Описание 2',
-    image: '/avatars/member2.jpg',
-  },
-  {
-    name: 'Имя 3',
-    role: 'Роль 3',
-    description: 'Описание 3',
     image: '/avatars/member3.jpg',
   },
   {
-    name: 'Имя 4',
-    role: 'Роль 4',
+    name: 'Штуккерт Герман',
+    role: 'Дизайнер',
+    description: 'Описание 3',
+    image: '/avatars/member2.jpg',
+  },
+  {
+    name: 'Гладченко Богдан',
+    role: 'Frontend',
     description: 'Описание 4',
     image: '/avatars/member4.jpg',
   },
   {
-    name: 'Имя 5',
-    role: 'Роль 5',
+    name: 'Мухин Егор',
+    role: 'Backend',
     description: 'Описание 5',
     image: '/avatars/member5.jpg',
   },
