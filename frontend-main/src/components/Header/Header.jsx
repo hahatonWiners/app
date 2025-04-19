3c84f3c25a3586d81815d4c563dffa487596447f
almond <almondawwawa@gmail.com>
2025-04-19 03:14:52 +0300

    Update links

--- frontend-main/src/components/Header/Header.jsx
+++ frontend-main/src/components/Header/Header.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      
+      if (scrollTop > lastScrollTop) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+      
+      setLastScrollTop(scrollTop);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollTop]);
+
   return (
-    <header className="header">
+    <header className={`header ${isHidden ? 'hidden' : ''}`}>
       <div className="header-content">
         <Link to="/" className="logo">
           NoScoffs
         </Link>
         <nav className="nav">
-          <Link to="/" className="nav-link">Главная</Link>
-          <Link to="/upload" className="upload-button">Загрузить данные</Link>
+          <Link to="/" className="main-button">Главная</Link>
+          <Link to="/about_us" className="nav-link">О нас</Link>
         </nav>
       </div>
     </header>
