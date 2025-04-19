1e0ee5aca9e3487b6867a165c18a6c09b9603bcf
almond <almondawwawa@gmail.com>
2025-04-19 06:20:40 +0300

    Add download and delete_temp endpoints

--- backend-main/main.py
+++ backend-main/main.py
@@ -4,6 +4,8 @@ import zipfile
 from pathlib import Path
 import tempfile
 
+from utils import calculate
+
 app = FastAPI()
 
 origins = ["*"]
@@ -27,51 +29,61 @@ def convert_to_csv(image_bytes: bytes, output_path: Path):
 def read_root():
     return {"message": "Приложение успешно запущено"}
     
-@app.get("/result")
-def result():
-    return {"message": "Yappy Dorress Za Loop a Naklo new noo yeah v leeeeee wow"}
-    
 @app.post("/upload")
 async def upload_data(a: int, b: int, c: int, d: int,  file: UploadFile = File(...)):
     # Проверяем, что загружен именно ZIP-файл
     if file.content_type not in ["application/zip", "application/x-zip-compressed"]:
         raise HTTPException(status_code=400, detail="Неверный тип файла. Поддерживаются только ZIP-архивы.")
 
-    with tempfile.TemporaryDirectory() as tempdir:
-        try:
-            # Сброс позиции на начало файла и распаковка
-            file.file.seek(0)
-            with zipfile.ZipFile(file.file) as zf:
-                for zip_info in zf.infolist():
-                    if zip_info.is_dir():
-                        continue  # Пропускаем директории
+    tempdir = tempfile.mkdtemp()
+    try:
+        # Сброс позиции на начало файла и распаковка
+        file.file.seek(0)
+        with zipfile.ZipFile(file.file) as zf:
+            for zip_info in zf.infolist():
+                if zip_info.is_dir():
+                    continue  # Пропускаем директории
 
-                    filename = Path(zip_info.filename)
-                    file_ext = filename.suffix.lower()
+                filename = Path(zip_info.filename)
+                file_ext = filename.suffix.lower()
 
-                    with zf.open(zip_info) as extracted_file:
-                        file_bytes = extracted_file.read()
+                with zf.open(zip_info) as extracted_file:
+                    file_bytes = extracted_file.read()
 
-                        if file_ext in [".jpg", ".jpeg", ".png"]:
-                            # Конвертируем изображение в CSV
-                            csv_filename = filename.stem + ".csv"
-                            output_path = f"{tempdir}/{csv_filename}"
-                            convert_to_csv(file_bytes, output_path)
-                        elif file_ext == ".csv":
-                            # Сохраняем CSV-файл напрямую
-                            output_path = f"{tempdir}/{filename.name}"
-                            with open(output_path, 'wb') as out_file:
-                                out_file.write(file_bytes)
-                        else:
-                            # Пропускаем файлы с неподдерживаемыми расширениями
-                            continue
+                    if file_ext in [".jpg", ".jpeg", ".png"]:
+                        # Конвертируем изображение в CSV
+                        csv_filename = filename.stem + ".csv"
+                        output_path = f"{tempdir}/{csv_filename}"
+                        convert_to_csv(file_bytes, output_path)
+                    elif file_ext == ".csv":
+                        # Сохраняем CSV-файл напрямую
+                        output_path = f"{tempdir}/{filename.name}"
+                        with open(output_path, 'wb') as out_file:
+                            out_file.write(file_bytes)
+                    else:
+                        # Пропускаем файлы с неподдерживаемыми расширениями
+                        continue
+        
+        output = calculate(tempdir)
+        output.to_csv(f"{tempdir}/{filename.name}")
 
-        except zipfile.BadZipFile:
-            raise HTTPException(status_code=400, detail="Повреждённый или недопустимый ZIP-архив.")
-        finally:
-            await file.close()
+    except zipfile.BadZipFile:
+        raise HTTPException(status_code=400, detail="Повреждённый или недопустимый ZIP-архив.")
+    finally:
+        await file.close()
 
-    return {"filename": file.filename, "message": f"Файлы успешно обработаны и сохранены в формате CSV"}
+    return {"filename": file.filename, "tempdir": tempdir, "message": f"Файлы успешно обработаны и сохранены в формате CSV"}
+
+@app.get("/download")
+def download(data: dict):
+    file_response = FileResponse(data["tempdir"] + "/result.csv")
+    shutil.rmtree(data["tempdir"])
+    return file_response
+
+@app.get("/delete_temp")
+def delete_temp(data: dict):
+    shutil.rmtree(data["tempdir"])
+    return {"message": "Temporary directory deleted"}
 
 @app.websocket("/ws")
 async def send(websocket: WebSocket):
